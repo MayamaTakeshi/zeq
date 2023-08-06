c18b5d531d1275619e0d9b050249c876db8bed6c
MayamaTakeshi <mayamatakeshi@gmail.com>
2023-08-06 09:06:04 +0900

    run_tests.sh: added error trap

--- run_tests.sh
+++ run_tests.sh
@@ -8,9 +8,20 @@ cd samples
 
 ok_tests=()
 
+current_test=""
+
+handle_error() {
+    echo
+    echo "An error occurred while executing $current_test"
+    exit 1
+}
+
+trap 'handle_error' ERR
+
 count=0
 for i in *.{js,mjs}
 do
+    current_test=$i
     node $i
     ok_tests+=("$i")
     count=$((count + 1))
